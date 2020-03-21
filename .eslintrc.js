f02aa4f47561401386ae526076315eca760993d0
David House <david.house@butcherbox.com>
2020-03-21 15:41:47 -0400

    Configure linting properly to run without errors and lint codebase

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 // cf: https://decodenatura.com/set-up-react-native-typescript-eslint-prettier/
 module.exports = {
-  extends: 'universe/native',
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  extends: [
+    'react-app',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/babel',
+    'prettier/react',
+    'prettier/standard',
+  ],
 }
